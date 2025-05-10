b72d1b5bd134db790fdbe3ea8628362a3db3025c
ManuelArango1229 <juan12292004@outlook.com>
2025-05-10 12:12:06 -0500

    Medición inicial del sensor implementada

--- app/src/main/java/com/miniproyecto/sensor_luz_ambiental/MainActivity.kt
+++ app/src/main/java/com/miniproyecto/sensor_luz_ambiental/MainActivity.kt
@@ -1,47 +1,71 @@
 package com.miniproyecto.sensor_luz_ambiental
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.miniproyecto.sensor_luz_ambiental.ui.theme.Sensor_luz_ambientalTheme
+import androidx.appcompat.app.AppCompatDelegate
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var brillo: Sensor? = null
+    private lateinit var texto: TextView
+    private lateinit var pb: CircularProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Sensor_luz_ambientalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        texto = findViewById(R.id.tv_text)
+        pb = findViewById(R.id.circularProgressBar)
+
+        setUpSensorStuff()
+    }
+
+    private fun setUpSensorStuff() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        brillo = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_LIGHT){
+            val luz = event.values[0]
+
+            texto.text = "Intesidad de la Luz: \n$luz\n\n${obtenerBrillo(luz)}"
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun obtenerBrillo(brillo: Float): String{
+        return when(brillo.toInt()){
+            0 -> "Oscuridad Total"
+            in 1 .. 10 -> "Oscuro"
+            in 11 .. 50 -> "Poco Oscuro"
+            in 51 .. 1000 -> "Normal"
+            in 1001 .. 5000 -> "Increiblemente Brillante"
+            else -> "Esta Luz Te Segara"
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Sensor_luz_ambientalTheme {
-        Greeting("Android")
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.registerListener(this, brillo, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager.unregisterListener(this)
     }
 }
