863b874dc057978ec7efed8c15efbaa99f81e2e5
ManuelArango1229 <juan12292004@outlook.com>
2025-05-10 11:00:24 -0500

    chore: inicial commit

---
+++ app/src/main/java/com/miniproyecto/sensor_luz_ambiental/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.miniproyecto.sensor_luz_ambiental
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.miniproyecto.sensor_luz_ambiental.ui.theme.Sensor_luz_ambientalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Sensor_luz_ambientalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sensor_luz_ambientalTheme {
+        Greeting("Android")
+    }
+}
